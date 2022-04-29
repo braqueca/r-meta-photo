f18436b9fccc8aa3b600f04becba7f11c5574db0
Billy Oswaldo Raquec Arias <braquec@gyt.com.gt>
2022-04-28 23:27:14 -0600

    Commit inicial

---
+++ src/main/java/com/braquec/rmetaphoto/RMetaPhotoApplication.java
@@ -0,0 +1,13 @@
+package com.braquec.rmetaphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RMetaPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RMetaPhotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/braquec/rmetaphoto/RMetaPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braquec.rmetaphoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RMetaPhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
